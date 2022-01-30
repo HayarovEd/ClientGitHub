7cd78e8e9bb279219d6935c8c7755ef4ccf30eed
HayarovEd <edurd@rambler.ru>
2022-01-30 19:10:09 +0500

    complete activity_main.xml

--- app/src/main/java/com/edurda77/ClientGitHub/ui/MainActivity.kt
+++ app/src/main/java/com/edurda77/ClientGitHub/ui/MainActivity.kt
@@ -2,11 +2,48 @@ package com.edurda77.ClientGitHub.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.edurda77.ClientGitHub.R
+import com.edurda77.ClientGitHub.databinding.ActivityMainBinding
+import com.edurda77.ClientGitHub.ui.model.UserModel
+import com.edurda77.filmlibrary.ui.UsersAdapter
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val userOfGitHub = emptyList<UserModel>().toMutableList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setRecycledView()
+    }
+    private fun setRecycledView() {
+        val recyclerView: RecyclerView = binding.usersRecyclerview
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        val users=initNotes()
+        val stateClickListener: UsersAdapter.OnStateClickListener =
+            object : UsersAdapter.OnStateClickListener {
+                override fun onStateClick(currentUser: UserModel, position: Int) {
+                    /*Thread {
+
+                        //val iDMovie = goIDMovie.getReposForIDMovieSync(movie)
+                        runOnUiThread {
+                            val intent = Intent(this@NotesActivity, NoteActivity::class.java)
+                            intent.putExtra(NoteMovie::class.java.simpleName, note)
+
+                            startActivity(intent)
+                        }
+                    }.start()*/
+                }
+            }
+        recyclerView.adapter = UsersAdapter(users,stateClickListener)
+    }
+    private fun initNotes(): List<UserModel>{
+        userOfGitHub.add(UserModel("HayarovEd"))
+        userOfGitHub.add(UserModel("kshalnov"))
+        userOfGitHub.add(UserModel("robertBadamshin"))
+        return userOfGitHub
     }
 }
---
+++ app/src/main/java/com/edurda77/ClientGitHub/ui/model/UserModel.kt
@@ -0,0 +1,5 @@
+package com.edurda77.ClientGitHub.ui.model
+
+data class UserModel(
+    val user: String
+)
