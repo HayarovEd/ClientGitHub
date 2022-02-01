d305328b8b0890a65c7cdaf375dfa0ac24cf3863
HayarovEd <edurd@rambler.ru>
2022-02-01 23:47:04 +0500

    start lesson 3

--- app/src/main/java/com/edurda77/ClientGitHub/domain/GitHubRepoUseCase.kt
+++ app/src/main/java/com/edurda77/ClientGitHub/domain/GitHubRepoUseCase.kt
@@ -3,9 +3,13 @@ package com.edurda77.ClientGitHub.domain
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import com.edurda77.ClientGitHub.model.RepoGitHubModel
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 
 interface GitHubRepoUseCase {
     @WorkerThread
     @Throws(Throwable::class)
     fun getReposForGitHub(userName: String): List<RepoGitHubModel>?
+    fun getReposObservable(userName: String): List<RepoGitHubModel>
+
 }
--- app/src/main/java/com/edurda77/ClientGitHub/model/RetrofitGitHubUseCaseImpl.kt
+++ app/src/main/java/com/edurda77/ClientGitHub/model/RetrofitGitHubUseCaseImpl.kt
@@ -2,6 +2,13 @@ package com.edurda77.ClientGitHub.model
 
 import com.edurda77.ClientGitHub.domain.GitHubRepoApi
 import com.edurda77.ClientGitHub.domain.GitHubRepoUseCase
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.internal.util.HalfSerializer.onError
+import io.reactivex.rxjava3.internal.util.HalfSerializer.onNext
+import io.reactivex.rxjava3.internal.util.NotificationLite.subscription
+import io.reactivex.rxjava3.schedulers.Schedulers
+import org.reactivestreams.Subscriber
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,14 +16,22 @@ import retrofit2.converter.gson.GsonConverterFactory
 const val BASE_URL = "https://api.github.com/"
 
 class RetrofitGitHubUseCaseImpl : GitHubRepoUseCase {
+    val rxAdapter = RxJava3CallAdapterFactory.createWithScheduler(Schedulers.io())
     private var retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .addCallAdapterFactory(rxAdapter)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
     private var api = retrofit.create(GitHubRepoApi::class.java)
 
     override fun getReposForGitHub(userName: String): List<RepoGitHubModel>? {
         return api.listRepos(userName).execute().body()
+    }
+
+    override fun getReposObservable(userName: String): List<RepoGitHubModel> {
+        api.listReposObservable(userName)
+            .flatMap {  }
+
     }
 
 
